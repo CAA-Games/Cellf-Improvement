6b46b49f815daf0ccfbd2aa65ecc0cb335d3c0b2
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-09-09 18:30:38 +0300

    learning Unity3d

---
+++ Cool-Alien-Agent/Assets/TestCubeBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCubeBehavior : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 v = new Vector2 (2,2); 
+		this.gameObject.transform.Rotate(v);
+
+	}
+}
