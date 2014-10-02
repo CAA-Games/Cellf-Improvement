df165d192020516c52c5fcf8edba9bd22c6138ae
41407 <jimi.welling@cs.helsinki.fi>
2014-10-02 20:46:25 +0300

    Basic powerup pickup

--- Cool-Alien-Agent/Assets/Scripts/PlasmidLogic.cs
+++ Cool-Alien-Agent/Assets/Scripts/PlasmidLogic.cs
@@ -1,24 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlasmidLogic : MonoBehaviour {
+public class PlasmidLogic : MonoBehaviour
+{
 
-	public Color color = Color.green;
+		private ArrayList plasmidEffects;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+		// Use this for initialization
+		void Start ()
+		{
 
-	void OnCollisionEnter2D(Collision2D col){
-		if(col.gameObject.tag == "Bacterium"){
-			col.gameObject.renderer.material.color = color;
-			Destroy (gameObject);
+
+				plasmidEffects = new ArrayList ();
+				
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{		
+				if (plasmidEffects.Count > 0) {
+						foreach (string effect in plasmidEffects) {
+								print (effect);
+						}
+				
+				}
+		}
+
+		void OnCollisionEnter2D (Collision2D col)
+		{
+				if (col.gameObject.tag == "Plasmid") {
+						plasmidEffects.Add (col.gameObject.GetComponent<PlasmidEffect> ().getEffect ());
+						Destroy (col.gameObject);
+				}
+
+		}
 }
