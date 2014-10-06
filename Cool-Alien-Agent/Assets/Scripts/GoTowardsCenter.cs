f3ef0cd0b065a570dec82ab5d0d9ecf1f6877776
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-10-06 18:13:07 +0300

    new way to present and control bacteria

---
+++ Cool-Alien-Agent/Assets/Scripts/GoTowardsCenter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoTowardsCenter : MonoBehaviour {
+
+	public GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Physics2D.IgnoreCollision (this.collider2D, player.collider2D);
+		rigidbody2D.AddForce (-1 * gameObject.transform.localPosition);
+	}
+}
---
+++ Cool-Alien-Agent/Assets/Scripts/MoveToClick1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToClick1 : MonoBehaviour {
+
+	private Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			print ("updating position");
+		}
+		Vector2 distance = targetPos - transform.position;
+		if (distance.magnitude > 1) {
+			distance = distance.normalized;
+		}
+
+		transform.Translate(distance * 2.0f * Time.deltaTime);
+	}
+}
