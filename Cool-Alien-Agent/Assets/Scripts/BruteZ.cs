a3b4dc1f09eeb11424adc927aee10e56f8b5af02
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-10-14 19:41:47 +0300

    moved script

---
+++ Cool-Alien-Agent/Assets/Scripts/BruteZ.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BruteZ : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 p = transform.position;
+		transform.position = new Vector3 (p.x, p.y, 0f);
+	}
+}
