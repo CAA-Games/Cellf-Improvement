d377c4f326995ebdcd34cd66f3d94eb13dfc602f
41407 <jimi.welling@cs.helsinki.fi>
2014-10-19 20:44:57 +0300

    Add placeholder cell appearance script

---
+++ Cool-Alien-Agent/Assets/CellAppearance.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellAppearance : MonoBehaviour
+{
+		public float randomVariance;
+		// Use this for initialization
+		void Start ()
+		{
+				
+				Color c = renderer.material.color;
+				renderer.material.color = new Color (c.r + Random.Range (-randomVariance, randomVariance),
+		                                     c.b + Random.Range (-randomVariance, randomVariance),
+		                                     c.g + Random.Range (-randomVariance, randomVariance));
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
