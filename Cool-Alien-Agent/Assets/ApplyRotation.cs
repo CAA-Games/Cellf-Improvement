cfff74927c8cb40dad2e356c0736b45d72703118
41407 <jimi.welling@cs.helsinki.fi>
2014-10-23 21:42:37 +0300

    Add slowly rotating capsule in front of the camera. THIS MAKES SENSE GUYS

---
+++ Cool-Alien-Agent/Assets/ApplyRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyRotation : MonoBehaviour
+{
+
+		public Vector3 rotation;
+
+		void Update ()
+		{
+				transform.Rotate (rotation);
+		}
+}
