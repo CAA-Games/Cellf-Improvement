a7a5be1901098f1b7c98e9cdcba83ca908effdb4
41407 <jimi.welling@cs.helsinki.fi>
2014-10-23 21:28:56 +0300

    Add subtle throbbing glow effect

---
+++ Cool-Alien-Agent/Assets/Scripts/Effects/ScalePulse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalePulse : MonoBehaviour
+{
+		public float baseScale = 1;
+		public float pulseSizeDifference = 0.2f;
+		public float rate = 0.02f;
+		private float randomSeed = Random.Range (0, 60);
+
+		void Update ()
+		{
+				Vector3 scale = new Vector3 (baseScale, baseScale, baseScale) + Vector3.one * Mathf.Sin (randomSeed + Time.frameCount * rate) * pulseSizeDifference;
+				transform.localScale = scale;
+		}
+}
