394eb260ed776c762200514b786954c956481a56
41407 <jimi.welling@cs.helsinki.fi>
2014-10-23 21:01:05 +0300

    Add music fade in script

---
+++ Cool-Alien-Agent/Assets/MusicFadeIn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicFadeIn : MonoBehaviour
+{
+
+		public float startingVolume;
+		public float targetVolume;
+		public float fadeInTime;
+		private float deltaVolume;
+
+		// Use this for initialization
+		void Start ()
+		{
+				audio.volume = startingVolume;
+				deltaVolume = (targetVolume - startingVolume) / fadeInTime;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (audio.volume < targetVolume) {
+						audio.volume += deltaVolume * Time.deltaTime;
+				} else if (audio.volume > targetVolume) {
+						audio.volume = targetVolume;
+						this.enabled = false;
+				}
+		}
+}
