c5ea033bed56b55e6b8829c04adf71f581667923
41407 <jimi.welling@cs.helsinki.fi>
2014-10-25 18:41:09 +0300

    Add 3D cell gore

---
+++ Cool-Alien-Agent/Assets/ApplicationLogic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationLogic : MonoBehaviour
+{
+
+		public static bool isShuttingDown;
+
+		// Use this for initialization
+		void Start ()
+		{
+				isShuttingDown = false;
+		}
+
+		void OnApplicationQuit ()
+		{
+				isShuttingDown = true;
+		}
+}
