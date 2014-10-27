19058c78c14266d941de5beee148a50674382d76
41407 <jimi.welling@cs.helsinki.fi>
2014-10-27 18:13:03 +0200

    Add mouse pointer

---
+++ Cool-Alien-Agent/Assets/MousePointer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePointer : MonoBehaviour
+{
+
+		public bool showOSCursor;
+
+		void Start ()
+		{
+				Screen.showCursor = showOSCursor;
+		}
+
+		void Update ()
+		{
+				transform.position = ApplicationLogic.GetWorldPositionOnPlane (Input.mousePosition, 0);
+		}
+}
