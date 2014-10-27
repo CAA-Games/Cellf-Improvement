bf36c795d9e5ac02f255298a750edd1e4801a575
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-10-27 17:04:24 +0200

    semilarge prefab, glowing plasmids and spinning things. All by Laura 3 km over the Atlantic ocean

---
+++ Cool-Alien-Agent/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	public float speed = 1;
+	public float neg = 1;
+	
+	// Use this for initialization
+	void Start () {
+		neg = ApplicationLogic.minusOrPlus();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Rotate (new Vector3(0,0,speed*neg));
+	}
+}
