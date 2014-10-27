91f880289235ae3f684662ac82eca67bd3e6f5ea
41407 <jimi.welling@cs.helsinki.fi>
2014-10-27 22:36:45 +0200

    Separated plasmid spawn effect into a prefab of its own

--- Cool-Alien-Agent/Assets/Scripts/PlasmidLogic.cs
+++ Cool-Alien-Agent/Assets/Scripts/PlasmidLogic.cs
@@ -7,6 +7,12 @@ public class PlasmidLogic : MonoBehaviour
 		public float gracePeriod = 3f;
 		public float currentGraceTime = 0f;
 		public bool canBePickedUp = false;
+		public GameObject spawnEffect;
+
+		void Start ()
+		{
+				Destroy (Instantiate (spawnEffect, transform.position, Quaternion.identity), 11.0f);
+		}
 
 		void Update ()
 		{
